4ba7ebd7d76659f74ee8093cc981ec2edf6991c5
Sugar <287276293@qq.com>
2026-08-11 16:01:13 +0800

    feat: 添加警戒带放置功能。

--- Assets/RSHE/Scripts/Core/Game/Props/Warning Post.cs
+++ Assets/RSHE/Scripts/Core/Game/Props/Warning Post.cs
@@ -1,16 +1,46 @@
 using Mirror;
+using UnityEngine;
 
+/// <summary>
+/// 道具：警戒桩
+/// </summary>
 public class WarningPost : NetworkBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    /// <summary> 当前世界位置 </summary>
+    private Vector3 _worldPos;
+
+    /// <summary> 生成的预制体 </summary>
+    [SerializeField] private GameObject _prefab;
+
+    /// <summary> 生成位置父类 </summary>
+    [SerializeField] private Transform _spawnParent;
+
+    #region 放置功能
+    /// <summary>
+    /// 生成放置物体
+    /// </summary>
+    public void Place()
     {
-        
+        CmdPlace();
     }
 
-    // Update is called once per frame
-    void Update()
+    [Command(requiresAuthority = false)]
+    private void CmdPlace()
     {
-        
+        // 服务器生成
+        Vector3 spawnPos = new Vector3(transform.position.x, -0.17f, transform.position.z);
+        GameObject newObj = Instantiate(_prefab, spawnPos, Quaternion.identity, _spawnParent);
+        NetworkIdentity newObjNetworkIdentity = newObj.GetComponent<NetworkIdentity>();
+        NetworkServer.Spawn(newObj);
+
+        // 客户端同步生成物体的父类
+        RpcAsyncParent(newObj);
     }
+
+    [ClientRpc] private void RpcAsyncParent(GameObject obj)
+    {
+        obj.transform.SetParent(_spawnParent);
+    }
+
+    #endregion
 }
